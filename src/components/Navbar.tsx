147453b305ebb2d30e93baaff2f536c6a5ce2a8b
MarufPulok <maruf12@student.sust.edu>
2024-03-02 03:02:29 +0600

    navbar UI WIP

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import React from "react";
+
+export default function Navbar() {
+  return <div className="fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b border-zinc-300 z-[10] py-2">
+    <div className="container mx-w-7xl h-full mx-auto flex items-center justify-between gap-2">
+        <Link href="/" className="flex gap-2 items-center">
+            <p className="hidden text-zinc-700 text-sm font-medium md:block">Crumbit</p>
+        </Link>
+    </div>
+  </div>;
+}
