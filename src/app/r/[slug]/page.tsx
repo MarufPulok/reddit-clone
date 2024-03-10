a8a411656dd7c95fab124a45456478107f02171a
MarufPulok <maruf12@student.sust.edu>
2024-03-11 01:04:17 +0600

    BREAKING CHANGES: logics and query for infinite scrolling

--- src/app/r/[slug]/page.tsx
+++ src/app/r/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import MiniCreatePost from "@/components/MiniCreatePost";
+import PostFeed from "@/components/PostFeed";
 import { INFINITE_SCROLLING_PAGINATION_RESULTS } from "@/config";
 import { getAuthSession } from "@/lib/auth";
 import { db } from "@/lib/db";
@@ -42,7 +43,7 @@ export default async function Page({ params }: PageProps) {
         r/{subreddit.name}
       </h1>
       <MiniCreatePost session={session} />
-      {/* TODO: show posts in user feed  */}
+      <PostFeed initialPosts={subreddit.posts} subredditName={subreddit.name} />
     </>
   );
 }
