94aa03510763150043db2be34099a17e908d4a6c
Me <maruf12@student.sust.edu>
2024-04-13 19:02:09 +0600

    Refactor CommentsSection and PostComment components

--- src/components/PostComment.tsx
+++ src/components/PostComment.tsx
@@ -8,6 +8,12 @@ import { Button } from "./ui/Button";
 import { MessageSquare } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { useSession } from "next-auth/react";
+import { Label } from "./ui/label";
+import { Textarea } from "./ui/textarea";
+import { useMutation } from "@tanstack/react-query";
+import { CommentRequest } from "@/lib/validators/comment";
+import axios from "axios";
+import { toast } from "@/hooks/use-toast";
 
 type ExtendedComment = Comment & {
   votes: CommentVote[];
@@ -31,6 +37,33 @@ export default function PostComment({
   const router = useRouter();
   const {data: session} = useSession()
   const [isReplying, setIsReplying] = useState<boolean>(false);
+  const [input, setInput] = useState<string>("");
+
+  const {mutate: postComment, isLoading} = useMutation({
+    mutationFn: async ({postId, text, replyToId}: CommentRequest) => {
+      const payload: CommentRequest = {
+        postId,
+        text,
+        replyToId,
+      };
+      
+      const {data} = await axios.patch("/api/subreddit/post/comment", payload);
+
+      return data
+    },
+    onError: () => {
+      return toast({
+        title: "Something went wrong",
+        description: "Comment wasn't posted successfully, please try again later",
+        variant: "destructive",
+      })
+    },
+    onSuccess: () => {
+      router.refresh()
+      setIsReplying(false)
+      setInput("")
+    }
+  })
 
   return (
     <div ref={commentRef} className="flex flex-col">
@@ -53,20 +86,51 @@ export default function PostComment({
       </div>
       <p className="text-sm text-zinc-900 mt-2">{comment.text}</p>
 
-      <div className="flex gap-2 items-center">
+      <div className="flex gap-2 items-center flex-wrap">
         <CommentVotes
           commentId={comment.id}
           initialVotesAmt={votesAmt}
           initialVote={currentVote}
         />
 
-        <Button onClick={() => {
-          if (!session) return router.push("/sign-in")
-            setIsReplying(true)
-        }} variant="ghost" size="xs">
+        <Button
+          onClick={() => {
+            if (!session) return router.push("/sign-in");
+            setIsReplying(true);
+          }}
+          variant="ghost"
+          size="xs"
+        >
           <MessageSquare className="h-4 w-4 mr-1.5" />
           Reply
         </Button>
+        {isReplying ? (
+          <div className="grid w-full gap-1.5">
+            <Label htmlFor="comment">Your comment</Label>
+            <div className="mt-2">
+              <Textarea
+                id="comment"
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+                rows={1}
+                placeholder="What are your thoughts?"
+              />
+              <div className="mt-2 flex justify-end gap-2">
+                <Button tabIndex={-1} variant="subtle" onClick={() => setIsReplying(false)}>Cancel</Button>
+                <Button
+                  isLoading={isLoading}
+                  disabled={input.length === 0}
+                  onClick={() => {
+                    if (!input) return;
+                    postComment({postId, text: input, replyToId: comment.replyToId ?? comment.id});
+                  }}
+                >
+                  Post
+                </Button>
+              </div>
+            </div>
+          </div>
+        ) : null}
       </div>
     </div>
   );
