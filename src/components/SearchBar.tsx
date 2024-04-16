bc9bc27bc4340c39daafa2fe987ded0ecedbdc1c
marufpulok <maruf12@student.sust.edu>
2024-04-16 23:16:38 +0600

    Refactor SearchBar component and import dependencies

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,49 +1,110 @@
 "use client";
-import React, { useState } from "react";
-import { Command, CommandEmpty, CommandGroup, CommandInput, CommandList } from "./ui/command";
+
+import { Prisma, Subreddit } from "@prisma/client";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
-import { Prisma, Subreddit } from "@prisma/client";
+import debounce from "lodash.debounce";
+import { usePathname, useRouter } from "next/navigation";
+import { FC, useCallback, useEffect, useRef, useState } from "react";
 
-export default function SearchBar() {
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command";
+import { useOnClickOutside } from "@/hooks/use-on-click-outside";
+import { Users } from "lucide-react";
+
+interface SearchBarProps {}
+
+const SearchBar: FC<SearchBarProps> = ({}) => {
   const [input, setInput] = useState<string>("");
+  const pathname = usePathname();
+  const commandRef = useRef<HTMLDivElement>(null);
+  const router = useRouter();
+
+  useOnClickOutside(commandRef, () => {
+    setInput("");
+  });
+
+  const request = debounce(async () => {
+    refetch();
+  }, 300);
+
+  const debounceRequest = useCallback(() => {
+    request();
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   const {
+    isFetching,
     data: queryResults,
     refetch,
     isFetched,
-    isFetching,
   } = useQuery({
     queryFn: async () => {
-      if (!input) return [];
-
+      if (input.length === 0) return [];
       const { data } = await axios.get(`/api/search?q=${input}`);
-
-      return data as Subreddit & {
+      return data as (Subreddit & {
         _count: Prisma.SubredditCountOutputType;
-      }[];
+      })[];
     },
     queryKey: ["search-query"],
     enabled: false,
   });
 
+  useEffect(() => {
+    setInput("");
+  }, [pathname]);
+
   return (
-    <Command className="relative rounded-lg border max-w-lg z-50 overflow-visible">
+    <Command
+      ref={commandRef}
+      className="relative rounded-lg border max-w-lg z-50 overflow-visible"
+    >
       <CommandInput
-        value={input}
+        // isLoading={isFetching}
         onValueChange={(text) => {
           setInput(text);
+          debounceRequest();
         }}
+        value={input}
         className="outline-none border-none focus:border-none focus:outline-none ring-0"
         placeholder="Search communities..."
-      ></CommandInput>
+      />
 
-      {input.length > 0 ? <CommandList className="absolute bg-white top-full inset-x-0 shadow rounded-b-md">
-        {isFetched && <CommandEmpty>No results found.</CommandEmpty>}
-        {(queryResults?.length ?? 0) > 0 ? (
-            <CommandGroup heading="Communities"></CommandGroup>
-        ): null} 
-      </CommandList> : null}
+      {input.length > 0 && (
+        <CommandList className="absolute bg-white top-full inset-x-0 shadow rounded-b-md">
+          {isFetched && queryResults && queryResults.length === 0 && (
+            <CommandEmpty>No results found.</CommandEmpty>
+          )}
+          {queryResults &&
+            Array.isArray(queryResults) &&
+            queryResults.length > 0 && (
+              <CommandGroup heading="Communities">
+                {queryResults.map((subreddit) => (
+                  <CommandItem
+                    onSelect={(e) => {
+                      router.push(`/r/${e}`);
+                      router.refresh();
+                    }}
+                    key={subreddit.id}
+                    value={subreddit.name}
+                  >
+                    <Users className="mr-2 h-4 w-4" />
+                    <a href={`/r/${subreddit.name}`}>{subreddit.name}</a>
+                  </CommandItem>
+                ))}
+              </CommandGroup>
+            )}
+        </CommandList>
+      )}
     </Command>
   );
-}
+};
+
+export default SearchBar;
